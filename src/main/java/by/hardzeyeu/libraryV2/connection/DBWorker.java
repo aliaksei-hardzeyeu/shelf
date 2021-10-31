c2f9c1fe2888253c80626867d30297c63b09d045
aliaksei-hardzeyeu <fugent89@yandex.ru>
2021-10-31 14:13:42 +0300

    minor changes, connection added

---
+++ src/main/java/by/hardzeyeu/libraryV2/connection/DBWorker.java
@@ -0,0 +1,29 @@
+package by.hardzeyeu.libraryV2.connection;
+
+import com.mysql.cj.jdbc.Driver;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBWorker {
+    private static final String URL = "jdbc:mysql://localhost:3306/andersen";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "root";
+
+    private Connection connection;
+
+    public DBWorker() {
+        try {
+            Driver driver = new Driver();
+            DriverManager.registerDriver(driver);
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+}
